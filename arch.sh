5be0b59bf3bc27f8b5bd4e02e95edddf24d431fe
robalozo <84756051+robalozo@users.noreply.github.com>
2021-05-26 10:35:15 -0300

    Add files via upload

---
+++ arch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+loadkeys br-abnt2
+timedatectl set-ntp true
+(
+    echo n
+    echo p
+    echo 1
+    echo 2048
+    echo 62914559
+    echo w
+) | fdisk /dev/sda
+
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt  
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+pacman -S nano grub networkmanager
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+hwclock --systohc
+locale-gen
+echo 'arch' > /etc/hostname
+echo '127.0.0.1	    localhost.localdomain   localhost' >> /etc/hosts
+echo '::1    localhost.localdomain   localhost' >> /etc/hosts
+echo '127.0.0.1	    arch.localdomain   arch' >> /etc/hosts
+
+mkinitcpio -P
+clear
+passwd
+
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+exit
+reboot
